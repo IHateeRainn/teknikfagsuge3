c1722b182eacc746a9643e7e8bb4083d713b5e9a
mira111111 <miranohr@gmail.com>
2026-09-15 12:22:30 +0200

    level

--- Game_Manager.gd
+++ Game_Manager.gd
@@ -1,6 +1,11 @@
 extends Node
 
-@onready var level_number = 1
+#Når et level er færdigt skriv: Game_Manager.level_number += 1
 
-func when_level_completed():
-	level_number += 1
+var level_number = 1:
+	set(value):
+		level_number = value
+		_on_level_completed_change(value)
+		 
+func _on_level_completed_change(value):
+		print(value)
