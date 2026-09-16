cc55d723b3652ef69717af7dfc5ce8d5841b6908
bertramhasseriisdietz-ship-it <bertramhasseriisdietz@gmail.com>
2026-09-16 11:37:45 +0200

    inventory done for real this time

--- inventory/Inventory_item.gd
+++ inventory/Inventory_item.gd
@@ -4,7 +4,7 @@ class_name InvItem
 
 @export_category("Identity")
 @export var item_name: String = ""
-@export var item_texture: Texture2D
+@export var texture: Texture2D
 
 @export_category("modifiers")
 @export var can_richochet: float
--- inventory/inv_ui.gd
+++ inventory/inv_ui.gd
@@ -1,18 +1,25 @@
 extends Control
 
+@onready var inv: Inv = preload("res://inventory/player_inventory.tres")
+@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+
 var is_open = false
 
-
 func _ready() -> void:
+	update_slots()
 	close()
 
 func _process(delta):
 	if Input.is_action_just_pressed("i"):
+		update_slots()
 		if is_open:
 			close()
 		else:
 			open()
 
+func update_slots():
+	for i in range(min(inv.items.size(), slots.size())):
+		slots[i].update(inv.items[i])
 
 func open():
 	visible = true
