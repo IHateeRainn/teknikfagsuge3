2b1d15e1a20bd9c5a8c324718aaaba2a547c42c1
mira111111 <miranohr@gmail.com>
2026-09-18 10:26:15 +0200

    woaw hoverhover

--- inventory/inv_ui_slot.gd
+++ inventory/inv_ui_slot.gd
@@ -1,5 +1,11 @@
 extends Panel
 
+func is_mouse_over() -> bool:
+	var local_mouse_pos = get_local_mouse_position()
+	return Rect2(Vector2.ZERO, size).has_point(local_mouse_pos)
+
+signal item_clicked(item)
+
 @onready var item_visual: Sprite2D = $CenterContainer/Panel/Sprite2D
 
 func update(item: InvItem):
@@ -8,3 +14,20 @@ func update(item: InvItem):
 	else:
 		item_visual.visible = true
 		item_visual.texture = item.texture
+
+var hovering: bool
+
+func _process(_delta):
+	if is_mouse_over():
+		hovering = true
+		scale = Vector2(1.2,1.2)
+	else:
+		hovering = false
+		scale = Vector2(1,1)
+
+
+func _input(event) -> void:
+	#Detects if mouse is hovering and clicks
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed and hovering:
+				item_clicked.emit()
